95238feb7b3429067fd58e2103de56781ac03d1f
faishalhmg <faishalhmg@yahoo.co.id>
2021-10-07 13:38:33 +0700

    praktikum 7 oktober

--- app/Controllers/Templating.php
+++ app/Controllers/Templating.php
@@ -3,9 +3,16 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\UserModel;
 
 class Templating extends BaseController
 {
+
+	public function __construct()
+	{
+		$this->userModel = new UserModel();
+	}
+
 	public function index()
 	{
 		$data = [
@@ -17,5 +24,27 @@ class Templating extends BaseController
 		// echo view('v_posts');
 		// echo view('layouts/footer');
 	return view('view_admin');
+    }
+	public function register()
+	{
+		$data = [
+			'title' => "Register",
+		];
+		// echo view('layouts/header',$data);
+		// echo view('layouts/navbar');
+		// echo view('v_posts');
+		// echo view('layouts/footer');
+	return view('v_register',$data);
+    }
+	public function saveRegister()
+	{
+		$request = service('request');
+		$data= [
+			'fullname' => $request -> getvar('fullname'),
+			'email' => $request -> getvar('email'),
+			'password' => $request -> getvar('password'),
+	];
+	$this->userModel->insert($data);
+	return redirect()->to('/register');
     }
 }
--- app/Views/view_admin.php
+++ app/Views/view_admin.php
@@ -1,7 +1,7 @@
 <?= $this->extend('template');?>
 <?= $this->section('content');?>
 <div class="wrapper">
-
+<body class="hold-transition sidebar-mini layout-fixed">
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
     <img class="animation__shake" src="/assets/adminlte/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
